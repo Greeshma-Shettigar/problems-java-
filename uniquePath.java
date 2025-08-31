8a42e456eadf7509af6f7ae0d4b1dae8423721ea
Greeshma Shettigar <shettigargreeshma@gmail.com>
2025-08-31 13:30:30 +0530

    Fix formatting in uniquePath.java comments for better readability

--- uniquePath.java
+++ uniquePath.java
@@ -1,4 +1,6 @@
-/*write a function that takes two input n and m and outputs the number of unique pathfrom top left corner to the bottom right corner of a n X m grid. constraint u can only move down or right one unit at a time  */
+/*write a function that takes two input n and m and outputs the number of unique pathfrom top left 
+corner to the bottom right corner of a n X m grid. constraint u can only move down or right one 
+unit at a time  */
 class uniquePath{
     static int uniquePath(int n,int m){
         if(n==1 || m==1){
